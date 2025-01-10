5f5dca3b4d811861e8a3b4c7d859462b9d05f2e5
erbdev <erbdev@hotmail.com>
2025-01-10 19:44:07 +0000

    Refactor landing section and home page; add slogan and scroll down component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
 "use client";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 import LandingSection from "@/sections/landing";
-// import Button from "@/components/ui/button";
-// import Card from "@/components/ui/card";
-// import Input from "@/components/ui/input";
-// import { FaArrowRight, FaUser } from "react-icons/fa";
-// import { GoHomeFill } from "react-icons/go";
-// import TextArea from "@/components/ui/text-area";
-// import Profile from "@/components/ui/profile";
-// import FancyButton from "@/components/ui/fancy-button";
-// import LiveClock from "@/components/ui/live-clock";
-// import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
-// import ScrollDown from "@/components/ui/scroll-down";
 
 export default function Home() {
   return (
@@ -26,41 +15,6 @@ export default function Home() {
           <LandingSection />
         </div>
       )}
-
-      {/* {() => (
-        <div className="w-full p-10">
-          <div className="max-w-2xl mx-auto">
-            <Card title="UI Components">
-              <div className="grid grid-cols-4">
-                <Button>Basic Button</Button>
-                <Button>
-                  <GoHomeFill />
-                  Basic Buttn
-                </Button>
-                <Button isIcon>
-                  <FaUser />
-                </Button>
-                <Button link="https://google.com"> Google</Button>
-              </div>
-              <Input type="text" placeholder="Enter your name" />
-              <Input
-                type="text"
-                placeholder="Enter your name"
-                icon={<FaUser />}
-              />
-              <TextArea placeholder="Enter your message" />
-              <Profile />
-              <MagneticWrapper className="w-[350px]">
-                <FancyButton text="Contact Me" icon={<FaArrowRight />} />
-              </MagneticWrapper>
-              <LiveClock timeZone="United Kingdom /London" />
-              <MagneticWrapper>
-                <ScrollDown />
-              </MagneticWrapper>
-            </Card>
-          </div>
-        </div>
-      )} */}
     </WaterWaveWrapper>
   );
 }
--- sections/landing.tsx
+++ sections/landing.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/navigation/header/header";
 import FancyButton from "@/components/ui/fancy-button";
 import LiveClock from "@/components/ui/live-clock";
+import ScrollDown from "@/components/ui/scroll-down";
 import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
 import { FaArrowRight } from "react-icons/fa";
 
 export default function LandingSection() {
   return (
-    <div className="relative h-screen overflow-hidden p-8">
+    <div className="relative h-screen overflow-hidden p-8 ">
       <Header />
 
       {/*Show magnetic fancy button on small screens and hide it on md screen */}
@@ -19,6 +20,35 @@ export default function LandingSection() {
       {/*Live Clock */}
       <div className="absolute bottom-10 right-10 ">
         <LiveClock timeZone="United Kingdom /London" />
+      </div>
+
+      {/*Slogan */}
+
+      <div className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8  leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[38rem] tracking-[-0.3rem]">
+        <div className="flex flex-col justify-center items-center   text-primary-foreground text-[18vw] lg:text-[14vw] 2xl:text-[12rem] font-normal uppercase ">
+          <div>
+            <span>Code</span>
+          </div>
+          <div>
+            <span>Crafting</span>
+          </div>
+          <div className="relative">
+            <span>Brilliance</span>
+            <div className="text-[1rem] leading-[1.4rem] tracking-[-0.07rem] absolute top-[14vw] lg:top-[10vw] 2xl:top-[9rem ] left-0 2xl:left-[57rem] w-[30rem] uppercase font-normal">
+              <span>Empowering innovation</span>
+              <br />
+              <span>through inspired design</span>
+              <br />
+              <span>and cutting-edge</span>
+              <br />
+              <span>and solutions redefine possibilities</span>
+            </div>
+          </div>
+        </div>
+        {/*Magentic scroll down */}
+        <MagneticWrapper className="absolute left-1/2 -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10">
+          <ScrollDown />
+        </MagneticWrapper>
       </div>
     </div>
   );
